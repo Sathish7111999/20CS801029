cb8a7ece0607b92146f546763d7a5d724f991802
Sathish7111999 <107687909+Sathish7111999@users.noreply.github.com>
2022-06-18 11:13:59 +0530

    latest code

---
+++ testtaken/src/main/java/com/api/automation/postspecificdata.java
@@ -0,0 +1,11 @@
+package com.api.automation;
+
+import com.intuit.karate.junit5.Karate;
+import com.intuit.karate.junit5.Karate.Test;
+
+public class postspecificdata {
+	@Test
+	public Karate runTest() {
+		return Karate.run("postdata").relativeTo(getClass());
+	}
+}
